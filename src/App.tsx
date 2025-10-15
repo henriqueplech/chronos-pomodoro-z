8fdfd88c4210e4824d4490c6e424a33802598f23
Henrique Ferrario <henriqueplech1@gmail.com>
2025-10-15 08:21:53 -0300

    testando

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
+
+import { Logo } from './components/Logo';
+import { Container } from './components/Container';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cumque
-        incidunt accusamus commodi quasi sit, doloribus in fugiat, repellat
-        laudantium voluptas praesentium labore dolore unde! Aliquam, dolorem! A,
-        quia rerum!
-      </p>
+      <Container>
+        <Logo />
+      </Container>
     </>
   );
 }
