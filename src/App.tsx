ad3d292946abf3b6fe45040a6049c9500f06dfa9
Henrique Ferrario <henriqueplech1@gmail.com>
2025-10-15 13:51:27 -0300

    Adicionando o logo, menu e coutdown

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,41 @@ import './styles/global.css';
 
 import { Logo } from './components/Logo';
 import { Container } from './components/Container';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
     <>
       <Container>
         <Logo />
+      </Container>
+
+      <Container>
+        <Menu></Menu>
+      </Container>
+
+      <Container>
+        <CountDown></CountDown>
+      </Container>
+
+      <Container>
+        <form className='form'>
+          <div className='formRow'>
+            <label htmlFor='meuInput'>Task</label>
+            <input id='meuInput' type='text' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>o o o o o o</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
